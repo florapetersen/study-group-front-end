67fbf576eed876ae7e8541473463b2ac32388939
florapetersen <floramarina@comcast.net>
2021-07-31 19:55:42 -0700

    add componentDidMount to courses index container

--- src/containers/CoursesIndexContainer.js
+++ src/containers/CoursesIndexContainer.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react'
+import CoursesList from '../components/CoursesList'
 
 export default class CoursesIndexContainer extends Component {
   state = {
-    groups: [],
+    courses: [],
     loading: true,
+  }
+
+  componentDidMount() {
+      setTimeout(() => {
+          this.setState({
+              courses: [
+                  { name: 'Biology' },
+                  { name: 'Chemistry' },
+              ],
+              loading: false,
+          })
+      }, 1000)
   }
 
   render() {
     return (
       <section className="max-w-6xl mx-auto mt-16">
         {this.state.loading
-          ? 'loading spinner'
-          : '<CoursesList courses={this.state.courses} />'}
+          ? ('loading spinner')
+          : (<CoursesList courses={this.state.courses} />)}
       </section>
     )
   }
