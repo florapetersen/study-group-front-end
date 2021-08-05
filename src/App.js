50140547e42de4e107f3413bd72e71f7adc7b6de
florapetersen <floramarina@comcast.net>
2021-08-04 19:47:29 -0700

    import NewNoteContainer to App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom'
 import CourseFormContainer from './containers/CourseFormContainer'
 import CoursesIndexContainer from './containers/CoursesIndexContainer'
+import NewNoteContainer from './containers/NewNoteContainer'
 
 function App() {
   return (
