76f070a63a5aa3dd725ff587a510d79b9f3ef1ec
florapetersen <floramarina@comcast.net>
2021-08-04 18:53:53 -0700

    added event listeners to CourseFormContainer.js

--- src/containers/CourseFormContainer.js
+++ src/containers/CourseFormContainer.js
@@ -9,14 +9,39 @@ export default class CourseFormContainer extends Component {
     school: ''
   }
 
+  handleChange = (e) => {
+      this.setState({
+          [e.target.name]: e.target.value,
+      })
+  }
+
+  handleSubmit = (e) => {
+      e.preventDefault()
+      fetch('http://localhost:3000/courses', {
+          method: 'POST',
+          headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ course: this.state }),
+      })
+      .then((res) => res.json())
+      .then((courseJson) => {
+        this.props.history.push('/courses')
+      })
+  }
+
   render() {
     return (
-      <form className="max-w-6xl w-3/4 mx-auto mt-16 shadow-lg px-4 py-6">
+      <form 
+        onSubmit={this.handleSubmit}
+        className="max-w-6xl w-3/4 mx-auto mt-16 shadow-lg px-4 py-6">
         <h1 className="text-center text-3xl font-semibold mb-2">New Course</h1>
         <fieldset>
           <input
             type="text"
             name="name"
+            onChange={this.handleChange}
             value={this.state.name}
             placeholder="Course name"
             className="w-full border p-4 my-4"
@@ -24,6 +49,7 @@ export default class CourseFormContainer extends Component {
           <input
             type="text"
             name="subject"
+            onChange={this.handleChange}
             value={this.state.subject}
             placeholder="Course subject"
             className="w-full border p-4 my-4"
@@ -31,6 +57,7 @@ export default class CourseFormContainer extends Component {
           <input
             type="text"
             name="instructor"
+            onChange={this.handleChange}
             value={this.state.instructor}
             placeholder="Course instructor"
             className="w-full border p-4 my-4"
@@ -38,6 +65,7 @@ export default class CourseFormContainer extends Component {
           <input
             type="text"
             name="year"
+            onChange={this.handleChange}
             value={this.state.year}
             placeholder="Year"
             className="w-full border p-4 my-4"
@@ -45,6 +73,7 @@ export default class CourseFormContainer extends Component {
           <input
             type="text"
             name="school"
+            onChange={this.handleChange}
             value={this.state.school}
             placeholder="School"
             className="w-full border p-4 my-4"
