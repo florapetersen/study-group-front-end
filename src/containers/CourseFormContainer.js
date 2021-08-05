25978e612a6befba1fb37af784373e70e1d0df28
florapetersen <floramarina@comcast.net>
2021-08-04 18:45:54 -0700

    updated CourseFormContainer.js

--- src/containers/CourseFormContainer.js
+++ src/containers/CourseFormContainer.js
@@ -2,17 +2,61 @@ import React, { Component } from 'react'
 
 export default class CourseFormContainer extends Component {
   state = {
-    groups: [],
-    loading: true,
+    name: '',
+    subject: '',
+    instructor: '',
+    year: '',
+    school: ''
   }
 
   render() {
     return (
-      <section className="max-w-6xl mx-auto mt-16">
-        {this.state.loading
-          ? 'loading spinner'
-          : '<CoursesList courses={this.state.courses} />'}
-      </section>
+      <form className="max-w-6xl w-3/4 mx-auto mt-16 shadow-lg px-4 py-6">
+        <h1 className="text-center text-3xl font-semibold mb-2">New Course</h1>
+        <fieldset>
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            placeholder="Course name"
+            className="w-full border p-4 my-4"
+          />
+          <input
+            type="text"
+            name="subject"
+            value={this.state.subject}
+            placeholder="Course subject"
+            className="w-full border p-4 my-4"
+          />
+          <input
+            type="text"
+            name="instructor"
+            value={this.state.instructor}
+            placeholder="Course instructor"
+            className="w-full border p-4 my-4"
+          />
+          <input
+            type="text"
+            name="year"
+            value={this.state.year}
+            placeholder="Year"
+            className="w-full border p-4 my-4"
+          />
+          <input
+            type="text"
+            name="school"
+            value={this.state.school}
+            placeholder="School"
+            className="w-full border p-4 my-4"
+          />
+        </fieldset>
+        <button
+          className="w-full p-4 bg-blue-300 mt-4 hover:bg-blue-400 transition-all duration-200"
+          type="submit"
+        >
+          Add Course
+        </button>
+      </form>
     )
   }
 }
