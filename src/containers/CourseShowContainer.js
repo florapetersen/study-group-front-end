fbe6686ccd2e08514a03c7d9a1b33311115eeec9
florapetersen <floramarina@comcast.net>
2021-08-05 18:35:23 -0700

    built out render method and fetch request in CourseShowContainer.js

--- src/containers/CourseShowContainer.js
+++ src/containers/CourseShowContainer.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react'
 
 export default class CourseShowContainer extends Component {
-  render() {
-    return <section>CourseShowContainer</section>
-  }
+    state = {
+        course: {},
+        loading: true,
+    }
+
+    componentDidMount() {
+        const courseId = this.props.match.params.courseId
+        fetch(`http://localhost:3001/courses/${courseId}`)
+        .then(res => res.json())
+        .then(({ course }) => {
+            this.setState({
+                course,
+                loading: false
+            })
+        })
+    }
+  
+    render() {
+        if (this.state.loading) {
+            return <div>Loading Spinner</div>
+        }
+        return <section className="max-w-6xl w-11/12 mx-auto mt-16">
+            <h1 className="text-3xl font-bold text-center">
+                {this.state.course.name}
+            </h1>
+        </section>
+    }
 }
