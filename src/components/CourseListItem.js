a2f90990b696fd5c2c9be0ddc6c53ea32d5488e3
florapetersen <floramarina@comcast.net>
2021-08-05 18:35:01 -0700

    added link to course in CourseListItem.js

--- src/components/CourseListItem.js
+++ src/components/CourseListItem.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const CourseListItem = ({ course }) => {
   return (
     <li className="" key={course.id}>
-      {course.name}
+      <Link to={`/courses/${course.id}`}>{course.name}</Link>
     </li>
   )
 }
